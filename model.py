94002c1f446b7681d513e58dfeb32bb83a2b6e52
Seema Ullal <seemaullal@gmail.com>
2022-01-15 21:35:53 -0800

    refactor to use class methods

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 
 db = SQLAlchemy()
 
+
 class Reservation(db.Model):
     __tablename__ = "reservations"
 
@@ -23,54 +24,60 @@ class Reservation(db.Model):
             "start_time": self.start_time.isoformat(),
         }
 
-def delete_reservations(reservation_start, username):
-    reservation_to_delete = Reservation.query\
-        .filter(Reservation.start_time==reservation_start)\
-        .filter(Reservation.username==username)\
-        .first()
-    db.session.delete(reservation_to_delete)
-    db.session.commit()
-    return reservation_to_delete
+    @classmethod
+    def find_reservation_by_start_and_user(cls, reservation_start, username):
+        reservation_to_delete = (
+            cls.query.filter(Reservation.start_time == reservation_start)
+            .filter(Reservation.username == username)
+            .first()
+        )
+        return reservation_to_delete
 
-def retrieve_reservations(username):
-    return Reservation.query.filter_by(username=username).all()
+    @classmethod
+    def get_all_for_user(cls, username):
+        return cls.query.filter_by(username=username).all()
 
-def create_reservation(username, reservation_start):
-    new_reservaton = Reservation(username=username, start_time=reservation_start)
-    db.session.add(new_reservaton)
-    db.session.commit()
+    @classmethod
+    def new_reservation_for_user(cls, username, reservation_start):
+        return cls(username=username, start_time=reservation_start)
 
-def available_reservations(start_time, end_time, username):
+    @classmethod
+    def find_available_reservations(cls, start_time, end_time, username):
         # retrieve reservations in within the specified time range
-    all_reservations_in_range = (
-        db.session.query(Reservation.start_time)\
-        .filter(Reservation.start_time.between(start_time, end_time))\
-    )
-    # get reservation times without time zone
-    existing_reservation_times = \
-        {res[0].replace(tzinfo=None) for res in all_reservations_in_range.all()}
+        all_reservations_in_range = db.session.query(cls.start_time).filter(
+            cls.start_time.between(start_time, end_time)
+        )
+        # get reservation times without time zone
+        existing_reservation_times = {
+            res[0].replace(tzinfo=None) for res in all_reservations_in_range.all()
+        }
 
-    # of exisitng reservations, get the ones with the user
-    user_reservations = all_reservations_in_range\
-        .filter(Reservation.username==username)\
-        .all()
-    user_reservation_dates = {res.start_time.date()for res in user_reservations}
+        # of existing reservations, get the ones with the user
+        user_reservations = all_reservations_in_range.filter(
+            cls.username == username
+        ).all()
+        user_reservation_dates = {res.start_time.date() for res in user_reservations}
 
-    # Initialize list for possible times
-    times = []
+        # Initialize list for possible times
+        times = []
 
-    # Possible reservations can only happen on the half hour
-    first_reservation_time = start_time + (datetime.min - start_time) \
-        % timedelta(minutes=30)
-    current = first_reservation_time
+        # Possible reservations can only happen on the half hour
+        first_reservation_time = start_time + (datetime.min - start_time) % timedelta(
+            minutes=30
+        )
+        current = first_reservation_time
 
-    # Add possible times, filtering where a reservation already exists OR where
-    # user already has a reservation on that date
-    while current < end_time:
-        if current not in existing_reservation_times and current.date() not in user_reservation_dates:
-            times.append(current)
-        current = current + timedelta(minutes=30)
-    return times
+        # Add possible times, filtering where a reservation already exists OR where
+        # user already has a reservation on that date
+        while current < end_time:
+            if (
+                current not in existing_reservation_times
+                and current.date() not in user_reservation_dates
+            ):
+                times.append(current)
+            current = current + timedelta(minutes=30)
+        return times
+
 
 def connect_to_db(flask_app):
     db_uri = os.environ["DATABASE_URL"].replace("postgres", "postgresql")
@@ -81,6 +88,7 @@ def connect_to_db(flask_app):
     db.init_app(flask_app)
 
     print("Connected to the db!")
+
 
 if __name__ == "__main__":
     app = Flask(__name__)
--- server.py
+++ server.py
@@ -1,18 +1,12 @@
 from flask import Flask, render_template, request, session, redirect, jsonify
 from dateutil.parser import parse
-from model import (
-    db,
-    connect_to_db,
-    available_reservations,
-    retrieve_reservations,
-    create_reservation,
-    delete_reservations,
-)
+from model import Reservation, db, connect_to_db
 import os
 
 app = Flask(__name__)
 app.secret_key = os.environ["APP_SECRET_KEY"]
 connect_to_db(app)
+
 
 @app.route("/")
 def homepage():
@@ -20,52 +14,65 @@ def homepage():
 
     return render_template("index.html")
 
+
 @app.route("/reservations", methods=["POST", "GET"])
 def get_user_reservations():
-    """ Retrieve reservations the user has made."""
+    """Retrieve reservations the user has made."""
     if request.method == "POST":
         username = request.form.get("username")
         session["username"] = username
     else:
         # if the user is not in session, redirect them to the homepage
-        if 'username' in session:
+        if "username" in session:
             username = session["username"]
         else:
             redirect("/")
-    existing_reservations = retrieve_reservations(username)
+    existing_reservations = Reservation.get_all_for_user(username)
 
     return render_template("reservations.html", reservations=existing_reservations)
+
 
 @app.route("/schedule")
 def render_schedule():
     """View scheduling page."""
     return render_template("schedule.html")
 
+
 @app.route("/reservations/delete", methods=["POST"])
 def delete_reservation():
-    """ Delete reservations the user has made."""
+    """Delete reservations the user has made."""
     reservation_start = parse(request.form.get("startTime"))
     username = session["username"]
 
-    reservation_to_delete = delete_reservations(reservation_start, username)
+    reservation_to_delete = Reservation.find_reservation_by_start_and_user(
+        reservation_start, username
+    )
+    db.session.delete(reservation_to_delete)
+    db.session.commit()
     return jsonify(reservation_to_delete.reservation_id)
+
 
 @app.route("/reservations/book", methods=["POST"])
 def make_reservation():
     """Create a reservation with the specified user and time."""
     reservation_start = parse(request.form.get("start_time"))
     username = session["username"]
-
-    create_reservation(username, reservation_start)
+    new_reservation = Reservation.new_reservation_for_user(username, reservation_start)
+    db.session.add(new_reservation)
+    db.session.commit()
     return redirect("/reservations")
+
 
 @app.route("/search_reservations", methods=["GET"])
 def search_reservation():
     start_time = parse(request.args.get("startTime"))
     end_time = parse(request.args.get("endTime"))
 
-    available_times = available_reservations(start_time, end_time, session["username"])
+    available_times = Reservation.find_available_reservations(
+        start_time, end_time, session["username"]
+    )
     return jsonify(available_times)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
